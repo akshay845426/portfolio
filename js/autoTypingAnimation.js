e44aab98f4f38ce629ac5582035ebbe9b7bda6ad
AKSHAY KUMAR GUPTA <136630543+akshay845426@users.noreply.github.com>
2024-04-12 23:07:46 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>I'm AKSHAY</span> I am a Full-Stack developer based in Bihar, India. I am an Information Technology undergraduate from B_TECH.<br><br> I am very passionate about improving my coding skills and developing applications and websites. I build WebApps and Websites. Working for myself to improve my skills. Love to build Full-Stack clones."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
